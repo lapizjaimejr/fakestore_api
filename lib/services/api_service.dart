455dcf7700d1cbcb7b183765b0211930a5f7d1e3
Jai <jaime.lapiz@usjr.edu.ph>
2022-07-12 23:13:56 +0800

    implemented loginProcess

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,20 @@
+import 'dart:convert';
+
+import '../models/user_credentials.dart';
+import 'package:http/http.dart' as http;
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<dynamic> processLogin(String userName, String pass) {
+    final userCredentials = UserCredentials(username: userName, password: pass);
+
+    return http
+        .post(Uri.parse('$baseUrl/auth/login'), body: userCredentials.toJson())
+        .then((data) {
+      if (data.statusCode == 200) {
+        return json.decode(data.body);
+      }
+    }).catchError((err) => print(err));
+  }
 }

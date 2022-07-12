131002088f72c993756a102bbcf63d637cd8e7d5
Jai <jaime.lapiz@usjr.edu.ph>
2022-07-12 23:45:47 +0800

    updated getIt implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 import 'screens/login_screen.dart';
+import 'services/api_service.dart';
+
+void setupLocator() {
+  GetIt.I.registerLazySingleton(() => ApiService());
+}
 
 void main() {
+  setupLocator();
   runApp(const MyApp());
 }
 
